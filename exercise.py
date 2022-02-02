aa92aaf73c9ccaa650f53c9ff55d841bb8e0f284
Stalon003 <78011967+Stalon003@users.noreply.github.com>
2022-02-02 15:40:09 +0300

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,9 @@
+#Arithmetic operators
+Age=input("Enter age:")
+Nationality=str(input("Enter nationality:"))
+if Nationality=="Kenyan"and age>=18:
+  
+  print ("eligible to vote")
+else:
+  print("not eligible to vote")
+  
