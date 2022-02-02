cd51d0b4ce9598959463f6b28479f286dd612d10
Stalon003 <78011967+Stalon003@users.noreply.github.com>
2022-02-02 14:56:19 +0300

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,10 @@
+#Arithmetic operators
+print("Arithmetic Operators")
+a=10
+b=5
+print("Addition:",a+b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor divisionn:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
