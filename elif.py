e257bc072831ed1421fe138f4d18a8547f9f71ab
Stalon003 <78011967+Stalon003@users.noreply.github.com>
2022-02-11 16:01:48 +0300

    using elif conditions to get the largest number

---
+++ elif.py
@@ -0,0 +1,9 @@
+A=int(input("Enter first number:"))
+B=int(input("Enter second number:"))
+C=int(input("Enter third number:"))
+if A>B and A>C:
+  print("largest",A)
+elif B>C and B>C:
+   print("largest",B)
+else:
+   print("largest",C)
